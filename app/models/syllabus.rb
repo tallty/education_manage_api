d7c5a5478972cdbea4fc3e9ce54aa87400932822
tangmingchao <18798805737@139.com>
2016-12-03 17:19:37 +0800

    edit syllabus_model,rb

--- app/models/syllabus.rb
+++ app/models/syllabus.rb
@@ -1,4 +1,5 @@
 class Syllabus < ActiveRecord::Base
   belongs_to :training_course
   belongs_to :school
+  has_many :signs, dependent: :destroy
 end
--- spec/models/syllabus_spec.rb
+++ spec/models/syllabus_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe Syllabus, type: :model do
   it { should belong_to(:school) } 
   it { should belong_to(:training_course) }
-  it { should have_many(:user_info) }
+  it { should have_many(:signs) }
 end
