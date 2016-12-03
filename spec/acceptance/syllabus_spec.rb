23cce6e94dd5d85e901ffe5c37c415054c35203b
tangmingchao <18798805737@139.com>
2016-12-03 23:05:34 +0800

    edit spec

--- spec/acceptance/syllabus_spec.rb
+++ spec/acceptance/syllabus_spec.rb
@@ -1,48 +1,17 @@
 require 'acceptance_helper'
 
-resource "管理员对 课程表 相关的API" do
+resource "学员对 课程表 相关的API" do
   header "Accept", "application/json"
-
-  #################### create #############################
-  post '/syllabuses' do
-    # admin_attrs = FactoryGirl.attributes_for(:admin)
-    syllabus_attrs = FactoryGirl.attributes_for(:syllabus)
-
-    # header "X-Admin-Token", admin_attrs[:authentication_token]
-    # header "X-Admin-Email", admin_attrs[:email]
-    parameter :title, " 课程表 课程的标题",required: true, scope: :syllabus
-    parameter :training_course_id, "课程表 课程 属于的项目",required: true, scope: :syllabus
-    parameter :school_id, "课程表 课程属于的 学校",required: true, scope: :syllabus
-    parameter :course_time, "课程表 课程的开课 时间",required: true, scope: :syllabus
-    parameter :content, "课程表 课程简介",required: true, scope: :syllabus
-
-    let(:title) {syllabus_attrs[:title]}
-    let(:training_course_id) {syllabus_attrs[:training_course_id]}
-    let(:school_id) {syllabus_attrs[:school_id]}
-    let(:course_time) {syllabus_attrs[:course_time]}
-    let(:content) {syllabus_attrs[:content]}
-
-    before do
-      # @admin = create(:admin)
-      # @school = create(:school)
-      # @training_course = create(:training_course)
-    end
-
-    example "管理员 创建课程表 成功" do
-      do_request
-      puts response_body
-      expect(status).to eq(201)
-    end
-  end
 
   ############### before_do ################################
   describe 'syllabus condition is all correct' do
-    # admin_attrs = FactoryGirl.attributes_for(:admin)
+    # user_attrs = FactoryGirl.attributes_for(:user)
 
-    # header "X-Admin-Token", admin_attrs[:authentication_token]
-    # header "X-Admin-Email", admin_attrs[:email]
+    # header "X-User-Token", user_attrs[:authentication_token]
+    # header "X-User-Email", user_attrs[:email]
 
     before do
+      # @user = create(:user)
       # @school = create(:school)
       @syllabuses = create_list(:syllabus, 5, course_time: Time.zone.now)
     end
@@ -56,7 +25,7 @@ resource "管理员对 课程表 相关的API" do
       let(:page) {1}
       let(:per_page) {6}
 
-      example "管理员 查询 课程表 记录 成功" do
+      example "学员 查询 课程表 记录 成功" do
         do_request
         puts response_body
         expect(status).to eq(200)
@@ -68,47 +37,10 @@ resource "管理员对 课程表 相关的API" do
 
       let(:id) {@syllabuses.first.id}
 
-      example " 管理员 查询指定 课程表 的详情成功" do
+      example " 学员 查询指定 课程表 的详情成功" do
         do_request
         puts response_body
         expect(status).to eq(200)
-      end
-    end
-    ##################### update #############################
-    put "/syllabuses/:id" do
-
-      let(:id) {@syllabuses.first.id}
-
-      syllabus_attrs = FactoryGirl.attributes_for(:syllabus)
-
-      parameter :title, " 课程表 课程的标题",required: true, scope: :syllabus
-      parameter :training_course_id, "课程表 课程 属于的项目",required: true, scope: :syllabus
-      parameter :school_id, "课程表 课程属于的 学校",required: true, scope: :syllabus
-      parameter :course_time, "课程表 课程的开课 时间",required: true, scope: :syllabus
-      parameter :content, "课程表 课程简介",required: true, scope: :syllabus
-
-      let(:title) {syllabus_attrs[:title]}
-      let(:training_course_id) {syllabus_attrs[:training_course_id]}
-      let(:school_id) {syllabus_attrs[:school_id]}
-      let(:course_time) {syllabus_attrs[:course_time]}
-      let(:content) {syllabus_attrs[:content]}
-
-      example " 管理员 修改指定 课程表 成功" do
-        do_request
-        puts response_body
-        expect(status).to eq(201)
-      end
-    end
-
-    ##################### delete #############################
-    delete "/syllabuses/:id" do
-
-      let(:id) {@syllabuses.first.id}
-
-      example " 管理员 删除指定 课程表 成功" do
-        do_request
-        puts response_body
-        expect(status).to eq(204)
       end
     end
 
