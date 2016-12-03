48bf4d958319fc61edeb258dec64482bc49012f5
tangmingchao <18798805737@139.com>
2016-12-03 17:25:47 +0800

    edit spec/factories/signs.rb

--- spec/factories/signs.rb
+++ spec/factories/signs.rb
@@ -13,9 +13,9 @@
 
 FactoryGirl.define do
   factory :sign do
-    training_course nil
-    school nil
-    user nil
-    sign_time "2016-12-03 17:15:43"
+    training_course 1
+    school 1
+    user 1
+    sign_time "#{Time.zone.now}"
   end
 end
