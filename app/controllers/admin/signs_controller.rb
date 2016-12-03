f1dbcd38e024a30914fa7120ec78472ca3228a48
tangmingchao <18798805737@139.com>
2016-12-03 22:21:06 +0800

    edit app/controllers/admin/signs_controller.rb

--- app/controllers/admin/signs_controller.rb
+++ app/controllers/admin/signs_controller.rb
@@ -1,10 +1,12 @@
 class Admin::SignsController < ApplicationController
-  before_action :set_admin_sign, only: [:show, :edit, :update, :destroy]
+  before_action :set_admin_sign, only: [:show]
 
-  respond_to :html
+  respond_to :html, :json
 
   def index
-    @admin_signs = Admin::Sign.all
+    page = params[:page] || 1
+    per_page = params[:per_page]
+    @admin_signs = Sign.all.paginate(page: page, per_page: per_page)
     respond_with(@admin_signs)
   end
 
@@ -12,36 +14,8 @@ class Admin::SignsController < ApplicationController
     respond_with(@admin_sign)
   end
 
-  def new
-    @admin_sign = Admin::Sign.new
-    respond_with(@admin_sign)
-  end
-
-  def edit
-  end
-
-  def create
-    @admin_sign = Admin::Sign.new(admin_sign_params)
-    @admin_sign.save
-    respond_with(@admin_sign)
-  end
-
-  def update
-    @admin_sign.update(admin_sign_params)
-    respond_with(@admin_sign)
-  end
-
-  def destroy
-    @admin_sign.destroy
-    respond_with(@admin_sign)
-  end
-
   private
     def set_admin_sign
-      @admin_sign = Admin::Sign.find(params[:id])
-    end
-
-    def admin_sign_params
-      params[:admin_sign]
+      @admin_sign = Sign.find(params[:id])
     end
 end
