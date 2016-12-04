020769e7bc7f5d737427eb1dec2e21534767d299
tangmingchao <18798805737@139.com>
2016-12-04 14:11:30 +0800

    add column 'authentication_token' to admins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204060724) do
+ActiveRecord::Schema.define(version: 20161204061001) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -59,8 +59,10 @@ ActiveRecord::Schema.define(version: 20161204060724) do
     t.integer  "adminable_id",           limit: 4
     t.string   "adminable_type",         limit: 255
     t.integer  "school_id",              limit: 4
+    t.string   "authentication_token",   limit: 30
   end
 
+  add_index "admins", ["authentication_token"], name: "index_admins_on_authentication_token", unique: true, using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
   add_index "admins", ["username"], name: "index_admins_on_username", unique: true, using: :btree
 
