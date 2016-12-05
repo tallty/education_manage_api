50b6212c0b7f10610fa8af075a984f039f17b2cd
tangmingchao <18798805737@139.com>
2016-12-05 17:25:55 +0800

    edit spec/models/syllabus_spec.rb

--- spec/models/syllabus_spec.rb
+++ spec/models/syllabus_spec.rb
@@ -16,7 +16,6 @@
 require 'rails_helper'
 
 RSpec.describe Syllabus, type: :model do
-  it { should belong_to(:school) } 
   it { should belong_to(:training_course) }
   it { should have_many(:signs) }
 end
