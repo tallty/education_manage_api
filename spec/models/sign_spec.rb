a6ced19f22634dd2836efb336b389409edefda78
tangmingchao <18798805737@139.com>
2016-12-05 17:25:10 +0800

    edit spec/models/sign_spec.rb

--- spec/models/sign_spec.rb
+++ spec/models/sign_spec.rb
@@ -18,6 +18,5 @@ require 'rails_helper'
 
 RSpec.describe Sign, type: :model do
   it { should belong_to(:user) } 
-  it { should belong_to(:school) }
-  it { should belong_to(:training_course) }
+  it { should belong_to(:syllabus) }
 end
