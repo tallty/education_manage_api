ce3f85532b502ac863adb94a0f549c8af65a7553
liyijie <liyijie825@gmail.com>
2016-12-06 23:56:32 +0800

    add load_and_authorize_resource to AdminPanel::ProjectsController

--- app/controllers/admin_panel/projects_controller.rb
+++ app/controllers/admin_panel/projects_controller.rb
@@ -1,4 +1,6 @@
 class AdminPanel::ProjectsController < AdminPanel::BaseController
+  load_and_authorize_resource
+  
   def index
     @projects = TrainingCourse.all.page(params[:page]).per(15)
   end
