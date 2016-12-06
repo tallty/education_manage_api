ec470e034d41e3eedce57c148ffbda68dbbeb7aa
liyijie <liyijie825@gmail.com>
2016-12-07 00:35:06 +0800

    fix Syllabus admin error

--- app/controllers/admin_panel/syllabuses_controller.rb
+++ app/controllers/admin_panel/syllabuses_controller.rb
@@ -2,7 +2,6 @@ class AdminPanel::SyllabusesController < AdminPanel::BaseController
   load_and_authorize_resource
 
   def index
-    @projects = TrainingCourse.all.page(params[:page]).per(15)
     @syllabuses = Syllabus.all.page(params[:page]).per(15)
   end
 end
