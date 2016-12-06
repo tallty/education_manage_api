8c7a828c3e0c96585dc5a58503954f6156251055
liyijie <liyijie825@gmail.com>
2016-12-07 00:25:58 +0800

    syntex error fix

--- app/controllers/admin_panel/syllabuses_controller.rb
+++ app/controllers/admin_panel/syllabuses_controller.rb
@@ -3,6 +3,6 @@ class AdminPanel::SyllabusesController < AdminPanel::BaseController
 
   def index
     @projects = TrainingCourse.all.page(params[:page]).per(15)
-    @syllabuses = Syllabuses.all.page(params[:page]).per(15)
+    @syllabuses = Syllabus.all.page(params[:page]).per(15)
   end
 end
