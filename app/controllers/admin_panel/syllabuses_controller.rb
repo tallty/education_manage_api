571e3a7b70005ef73b178ff16bfa89e205a5d9d5
liyijie <liyijie825@gmail.com>
2016-12-07 00:22:20 +0800

    add @syllabuses to controller

--- app/controllers/admin_panel/syllabuses_controller.rb
+++ app/controllers/admin_panel/syllabuses_controller.rb
@@ -3,5 +3,6 @@ class AdminPanel::SyllabusesController < AdminPanel::BaseController
 
   def index
     @projects = TrainingCourse.all.page(params[:page]).per(15)
+    @syllabuses = Syllabuses.all.page(params[:page]).per(15)
   end
 end
