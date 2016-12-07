fb8178593003e152a5ef090688154aeea2cfffd6
tangmingchao <18798805737@139.com>
2016-12-07 14:13:50 +0800

    edit app/models/sign.rb#add valadate and def_get_user

--- app/models/sign.rb
+++ app/models/sign.rb
@@ -21,17 +21,20 @@ class Sign < ActiveRecord::Base
   ################validates#####################
   validates_presence_of :syllabus_id, on: :create, message: " syllabus_id不能为空"
   validates_presence_of :user_id, on: :create, message: "user_id不能为空"
-  # validates_presence_of :title, on: :create, message: "title不能为空"
-  # validates_presence_of :teacher, on: :create, message: " teacher不能为空"
-  # validates_presence_of :address, on: :create, message: "address不能为空" 
 
-  scope :default_order, -> { order("created_at DESC") }
+  validates_uniqueness_of :syllabus_id, on: :create, message: "您已经签到过了，不能重复签到 ！"
 
-  def date
+  scope :default_order, -> { order("created_at DESC") } #降序
+
+  def self.get_user(id)#判断当前登录的学员是否报名了该培训项目
+    self.syllabus.training_course.user_training_courses.where(user_id:id)
+  end
+
+  def date #签到日期
    	self.created_at.strftime("%Y-%m-%d")
   end 
 
-  def time
+  def time#签到时间
   	self.created_at.strftime("%H:%M:%S")
   end
 end
